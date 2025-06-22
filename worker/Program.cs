946738638a0fcce0a08932360a8189fcef2086b7
Antonella Brochini <antobrochini97@gmail.com>
2025-06-22 19:25:37 -0300

    funciona

--- worker/Program.cs
+++ worker/Program.cs
@@ -121,7 +121,6 @@ namespace Worker
                 HttpClient httpClient = new System.Net.Http.HttpClient();
 
                 var content = new System.Net.Http.StringContent(json, System.Text.Encoding.UTF8, "application/json");
-                string hardcode = "https://priweitlgc.execute-api.us-east-1.amazonaws.com/prod/voting_result";
                 var response = await httpClient.PostAsync(url, content);
                 response.EnsureSuccessStatusCode();
 
